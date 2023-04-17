55ad5d2bb63f91d3f7291fbac5f47f2d9f64b971
IremTektas <tektasirem13@gmail.com>
2023-04-17 18:33:07 +0300

    designed first page

--- DutyPharmacy/DutyPharmacy/View/ViewController.swift
+++ DutyPharmacy/DutyPharmacy/View/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+   
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var districtTextField: UITextField!
+    
+    var cityPickerView = UIPickerView()
+    var districtPickerView = UIPickerView()
     
     private var pharmacyViewModel = PharmacyViewModel()
+    
+    
+    
+    let cities = ["Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Isparta", "Mersin", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"]
 
+
+    var  districts = ["test1","test2"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cityTextField.inputView = cityPickerView
+        districtTextField.inputView = districtPickerView
+        
+        cityPickerView.delegate = self
+        cityPickerView.dataSource = self
+        
+        districtPickerView.delegate = self
+        districtPickerView.dataSource = self
+        
+        cityPickerView.tag = 1
+        districtPickerView.tag = 2
         
         pharmacyViewModel.fetchPharmacyData { [weak self] result in
             DispatchQueue.main.async {
@@ -30,13 +55,55 @@ class ViewController: UIViewController {
     }
     
  
-    
-   
    
     @IBAction func tıkl(_ sender: Any) {
         
         performSegue(withIdentifier: "toPharmacyPage", sender: nil)
     }
     
+    
+    
+    
 }
 
+extension ViewController: UIPickerViewDelegate , UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return cities.count
+        case 2:
+            return districts.count
+        default:
+            return 1
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1:
+            return cities[row]
+        case 2:
+            return districts[row]
+        default:
+            return "no data"
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 1:
+            cityTextField.text = cities[row]
+            cityTextField.resignFirstResponder()
+        case 2:
+            districtTextField.text = districts[row]
+            districtTextField.resignFirstResponder()
+        default:
+            return
+        }
+    }
+    
+}
