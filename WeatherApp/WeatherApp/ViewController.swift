c9cad2a7f7f79061b6ce9a6e8edec3cd65bbf0fc
IremTektas <tektasirem13@gmail.com>
2023-05-06 21:49:04 +0300

    initial commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by iremt on 6.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
