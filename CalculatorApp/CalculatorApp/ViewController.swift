5715fad3f84abcab72664dc16c1d89642ed50aa1
IremTektas <tektasirem13@gmail.com>
2023-07-07 22:55:01 +0300

    initial commit

---
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by iremt on 7.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
